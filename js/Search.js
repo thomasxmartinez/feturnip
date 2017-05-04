66adfbe677405ac3062f1e1c726ecfbcdd2ce580
tommytnez <tommytnez@yahoo.com>
2017-05-03 23:06:29 -0700

    header

--- js/Search.js
+++ js/Search.js
@@ -3,13 +3,25 @@ import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     return (
       <div className='search'>
+        <header>
+          <h1>{this.state.searchTerm}</h1>
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
+        </header>
         <div>
           {preload.shows.map((show) => {
             return (
-              <ShowCard key={show.imdbID} show={show} />
+              <ShowCard key={show.imdbID} {...show} />
             )
           })}
         </div>
