7c8d67500078dc238f97cf798e5920354198c138
tommytnez <tommytnez@yahoo.com>
2017-05-05 13:04:57 -0700

    proj

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -1,25 +1,29 @@
+/* eslint-env node, jest */
 import React from 'react'
-import { shallow } from 'enzyme'
-import { shallowToJson } from 'enzyme-to-json'
-import Search from './Search'
+import { Provider } from 'react-redux'
+import Search, { Unwrapped as UnwrappedSearch} from './Search'
 import ShowCard from './ShowCard'
+import store from './store'
+import { setSearchTerm } from './actionCreators'
+import { shallow, render } from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
 import preload from '../public/data.json'
 
-test('Search snapshot test', () => {
-  const component = shallow(<Search />)
+test('Search snapshot', () => {
+  const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm='' />)
   const tree = shallowToJson(component)
   expect(tree).toMatchSnapshot()
 })
 
-test('Search should render a ShowCard for each show', () => {
-  const component = shallow(<Search />)
-  expect(component.find(ShowCard).length).toEqual(preload.shows.length)
+test('Search should render all shows initially', () => {
+  const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm='' />)
+  expect(preload.shows.length).toEqual(component.find(ShowCard).length)
 })
 
 test('Search should render correct amount of shows based on search', () => {
   const searchWord = 'house'
-  const component = shallow(<Search />)
-  component.find('input').simulate('change', {target: {value: searchWord}})
-  const showCount = preload.shows.filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0).length
-  expect(component.find(ShowCard).length).toEqual(showCount)
+  store.dispatch(setSearchTerm(searchWord))
+  const component = render(<Provider store={store}><Search shows={preload.shows} /></Provider>)
+  const showCount = preload.shows.filter((show) => `${show.title.toUpperCase()} ${show.description.toUpperCase()}`.includes(searchWord.toUpperCase())).length
+  expect(showCount).toEqual(component.find('.show-card').length)
 })
--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,27 @@ const path = require('path')
 module.exports = {
   context: __dirname,
   entry: './js/ClientApp.js',
-  devtool: 'eval',
+  devtool: 'cheap-module-source-map',
   output: {
     path: path.join(__dirname, '/public'),
+    publicPath: '/public/',
     filename: 'bundle.js'
   },
-  devServer: {
-    publicPath: '/public/',
-    historyApiFallback: true
-  },
   resolve: {
-    extensions: ['.js', '.json']
+    // alias: {
+    //   react: 'preact-compat',
+    //   'react-dom': 'preact-compat'
+    // },
+    extensions: ['.js', '.jsx', '.json']
   },
   stats: {
     colors: true,
     reasons: true,
-    chunks: true
+    chunks: false
+  },
+  devServer: {
+    publicPath: '/public/',
+    historyApiFallback: true
   },
   module: {
     rules: [
@@ -27,15 +32,6 @@ module.exports = {
         test: /\.js$/,
         loader: 'eslint-loader',
         exclude: /node_modules/
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
-      },
-      {
-        include: path.resolve(__dirname, 'js'),
-        test: /\.js$/,
-        loader: 'babel-loader'
       },
       {
         test: /\.css$/,
@@ -48,6 +44,18 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        include: [
+          path.resolve('js'),
+          path.resolve('node_modules/preact-compat/src')
+        ]
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
       }
     ]
   }
