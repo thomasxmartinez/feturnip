36b2316b039f3bb3bb3614d87ad60e6f6d12b5c5
tommytnez <tommytnez@yahoo.com>
2017-05-06 16:16:37 -0700

    Migrate from create-react-app-buildpack to Node server

--- react-ui/js/ClientApp.js
+++ react-ui/js/ClientApp.js
@@ -3,4 +3,8 @@ import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router'
 import App from './App'
 
-render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('app'))
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('app'))
