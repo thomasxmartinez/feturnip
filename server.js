77121acac6b8a81df89341c26d8ddff79b6338d5
tommytnez <tommytnez@yahoo.com>
2017-05-07 11:38:42 -0700

    yuop

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const babelRegister = require('babel-register')
+require('babel-register')
 
 const express = require('express')
 const React = require('react')
@@ -28,20 +28,5 @@ server.use((req, res) => {
   res.end()
 })
 
-// Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
-
-// Answer API requests.
-app.get('/api', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send('{"message":"Hello from the custom server!"}');
-});
-
-// All remaining requests return the React app, so it can handle routing.
-app.get('*', function(request, response) {
-  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-});
-
-app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-});
+console.log('listening on port', PORT)
+server.listen(PORT)
